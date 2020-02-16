267e9c5a6e076f28cef87c7aeb90ec0188158d70
Hunter Kaleb Perry <skittles2519@hotmail.com>
2020-02-15 18:59:46 -0700

    Add project files.

---
+++ ASPLearning/Data/ASPLearningContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASPLearning.Models;
+
+namespace ASPLearning.Data
+{
+    public class ASPLearningContext : DbContext
+    {
+        public ASPLearningContext (DbContextOptions<ASPLearningContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPLearning.Models.Movie> Movie { get; set; }
+    }
+}
