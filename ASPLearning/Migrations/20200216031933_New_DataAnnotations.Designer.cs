63b1fc9eb9418e5a5053546f9e9bd6d4645f62da
Hunter Kaleb Perry <skittles2519@hotmail.com>
2020-02-15 20:20:59 -0700

    Finish

--- ASPLearning/Migrations/20200216031933_New_DataAnnotations.Designer.cs
+++ ASPLearning/Migrations/20200216031933_New_DataAnnotations.Designer.cs
@@ -4,14 +4,16 @@ using ASPLearning.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASPLearning.Migrations
 {
     [DbContext(typeof(ASPLearningContext))]
-    partial class ASPLearningContextModelSnapshot : ModelSnapshot
+    [Migration("20200216031933_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
